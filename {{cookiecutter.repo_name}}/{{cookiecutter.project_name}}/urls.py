f15ef9d4595ac7b0fda71c5995e364a324e4c2a8
Romain Gonçalves <commit@rgoncalves.se>
2021-09-06 11:26:43 +0200

    urls: Serve media and static files on debug
    Media files such as images can be saved on a development instance,
    without spinning up an S3 storage (cf. settings.py).

--- {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/urls.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.project_name}}/urls.py
@@ -5,9 +5,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/topics/http/urls/
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import (
+    include,
+    path
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('accounts.urls')),
 ]
+
+if settings.DEBUG:
+    for el in [
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    ]: urlpatterns += el  # noqa: E701
